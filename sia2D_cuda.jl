f0aaed5efe311c642d80393eef1bc9f81383ac17
Ludovic Raess <ludovic.rass@gmail.com>
2022-10-26 17:40:32 +0200

    More update

--- sia2D_cuda.jl
+++ sia2D_cuda.jl
@@ -15,7 +15,16 @@ macro d_ya_avx(A) esc(:( 0.5*(($A[ix,iy+1]-$A[ix,iy]) + ($A[ix+1,iy+1]-$A[ix+1,i
 
 CUDA.device!(7) # GPU selection
 
-function compute_diffusitivity!(S,gradS,D,H,n,a,dx,dy)
+function bc!(qHx,qHy,H)
+    @get_thread_idx(H)
+    if (ix<=nx+1 && iy==1   )  qHx[ix,iy] = qHx[ix,iy+1] end
+    if (ix<=nx+1 && iy==ny  )  qHx[ix,iy] = qHx[ix,iy-1] end
+    if (ix==1    && iy<=ny+1)  qHy[ix,iy] = qHy[ix+1,iy] end
+    if (ix==nx   && iy<=ny+1)  qHy[ix,iy] = qHy[ix-1,iy] end
+    return
+end
+
+function compute_diffus!(S,gradS,D,H,n,a,dx,dy)
     @get_thread_idx(S)
     if ix<=nx-1 && iy<=ny-1
         gradS[ix,iy] = sqrt((@d_xa_avy(S)/dx)^2 + (@d_ya_avx(S)/dy)^2)
@@ -26,72 +35,43 @@ end
 
 function compute_flux!(S,qHx,qHy,D,dx,dy)
     @get_thread_idx(S)
-    if ix<=nx-1 && iy<=ny-2
-        qHx[ix+1,iy+1] = -@av_ya(D)*@d_xi(S)/dx
-    end
-    if ix<=nx-2 && iy<=ny-1
-        qHy[ix+1,iy+1] = -@av_xa(D)*@d_yi(S)/dy
-    end
+    if (ix<=nx-1 && iy<=ny-2) qHx[ix+1,iy+1] = -@av_ya(D)*@d_xi(S)/dx end
+    if (ix<=nx-2 && iy<=ny-1) qHy[ix+1,iy+1] = -@av_xa(D)*@d_yi(S)/dy end
     return
 end 
 
-function compute_icethickness!(S,M,dHdt,qHx,qHy,dx,dy)
+function update_H_S!(dHdt,H,S,B,qHx,qHy,M,dt,dx,dy)
     @get_thread_idx(S)
-    if ix<=nx && iy<=ny
+    if (ix<=nx && iy<=ny)
         dHdt[ix,iy] = -(@d_xa(qHx)/dx + @d_ya(qHy)/dy) + M[ix,iy]
+        H[ix,iy]    = max(0.0, H[ix,iy] + dt*dHdt[ix,iy])
+        S[ix,iy]    = B[ix,iy] + H[ix,iy]
     end
-    return
-end 
-
-function update_H!(H,dHdt,dt)
-    @get_thread_idx(H)
-    if ix<=nx && iy<=ny
-        H[ix,iy] = max(0.0, H[ix,iy] + dt*dHdt[ix,iy])
-    end
-    return
-end 
-
-function update_S!(S,H,B)
-    @get_thread_idx(S)
-    if ix<=nx && iy<=ny
-        S[ix,iy] = B[ix,iy] + H[ix,iy]
-    end
-    return
-end
-
-function bc!(qHx,qHy,H)
-    @get_thread_idx(H)
-    if (ix<=nx+1 && iy==1   )  qHx[ix,iy] = qHx[ix,iy+1] end
-    if (ix<=nx+1 && iy==ny  )  qHx[ix,iy] = qHx[ix,iy-1] end
-    if (ix==1    && iy<=ny+1)  qHy[ix,iy] = qHy[ix+1,iy] end
-    if (ix==nx   && iy<=ny+1)  qHy[ix,iy] = qHy[ix-1,iy] end
     return
 end
 
 function update!(S,H,B,M,D,gradS,qHx,qHy,dHdt,dx,dy,dt,n,a,threads,blocks)
-    CUDA.@sync @cuda threads=threads blocks=blocks compute_diffusitivity!(S,gradS,D,H,n,a,dx,dy)
+    CUDA.@sync @cuda threads=threads blocks=blocks compute_diffus!(S,gradS,D,H,n,a,dx,dy)
     CUDA.@sync @cuda threads=threads blocks=blocks compute_flux!(S,qHx,qHy,D,dx,dy)
     CUDA.@sync @cuda threads=threads blocks=blocks bc!(qHx,qHy,H)
-    CUDA.@sync @cuda threads=threads blocks=blocks compute_icethickness!(S,M,dHdt,qHx,qHy,dx,dy)
-    CUDA.@sync @cuda threads=threads blocks=blocks update_H!(H,dHdt,dt)
-    CUDA.@sync @cuda threads=threads blocks=blocks update_S!(S,H,B)
+    CUDA.@sync @cuda threads=threads blocks=blocks update_H_S!(dHdt,H,S,B,qHx,qHy,M,dt,dx,dy)
     return
 end
 
 function sia_2D()
     # physics
     s2y     = 3600*24*365.25 # seconds to years
-    lx,ly   = 30e3, 30e3     # lx, ly = 30 km
+    lx,ly   = 30e3,30e3      # lx, ly = 30 km
     n       = 3
-    ρg      = 970*9.8
-    ttot    = 10e4
+    ρg      = 910*9.8
+    ttot    = 50e3
     a0      = 1.5e-24
     # numerics
     nx,ny   = 127,127
-    nout    = 2000    # error check frequency
-    ndt     = 50      # dt check/update
+    nout    = 5000    # error check frequency
+    ndt     = 100      # dt check/update
     threads = (16,16) # n threads
-    dtsc    = 0.5     # iterative dt scaling 
+    dtsc    = 0.7     # iterative dt scaling 
     epsi    = 1e-4
     ϵtol    = 1e-8
     # derived numerics
@@ -100,6 +80,7 @@ function sia_2D()
     xc      = LinRange(dx/2,lx-dx/2,nx)
     yc      = LinRange(dy/2,ly-dy/2,ny)
     cfl     = max(dx^2,dy^2)/4.1
+    blocks  = ceil.(Int,(nx,ny)./threads)
     # derived physics 
     a       = 2.0*a0/(n+2)*ρg^n*s2y
     # array initialisation
@@ -112,10 +93,11 @@ function sia_2D()
     M[xc.>xm ,:] .= 0.0 
     B[xc.<xmB,:] .= 500
     B[2:end-1,2:end-1] .= B[2:end-1,2:end-1] .+ 1.0./4.1.*(diff(diff(B[:,2:end-1], dims=1), dims=1) .+ diff(diff(B[2:end-1,:], dims=2), dims=2))
-    B[:,[1,end]] .= B[:,[2,end-1]]
-    # plot
-    p1 = heatmap(xc, yc, B', aspect_ratio=1, xlims=(xc[1], xc[end]), ylims=(yc[1], yc[end]), c=:turbo, title= "H")
-    p2 = heatmap(xc, yc, M', aspect_ratio=1, xlims=(xc[1], xc[end]), ylims=(yc[1], yc[end]), c=:turbo, title = "B")
+    B[:,[1,end]] .= B[:,[2,end-1]] # BCs
+    # visu
+    opts = (aspect_ratio=1,xlims=(xc[1],xc[end]),ylim=(yc[1],yc[end]),c=:turbo)
+    p1 = heatmap(xc,yc,B',title="H"; opts...)
+    p2 = heatmap(xc,yc,M',title="B"; opts...)
     display(plot(p1, p2))
     # array initialization 
     H     = CuArray{Float64}(H) 
@@ -126,18 +108,14 @@ function sia_2D()
     D     = CUDA.zeros(Float64,nx-1,ny-1)
     qHx   = CUDA.zeros(Float64,nx+1,ny  )
     qHy   = CUDA.zeros(Float64,nx  ,ny+1)
-    dHdt  = CUDA.zeros(Float64,nx  ,ny  )
-    blocks = ceil.(Int,(nx,ny)./threads)
-    opts = (aspect_ratio=1,xlims=(xc[1],xc[end]),ylim=(yc[1],yc[end]),c=:turbo)
-    # init bed
-    CUDA.@sync @cuda threads=threads blocks=blocks update_S!(S,H,B)
+    dHdt  = CUDA.zeros(Float64,nx  ,ny  )    
+    S    .= B .+ H # init bed
     t = 0.0; it = 1; dt = dtsc * min(1.0, cfl/(epsi+maximum(D)))
     while t <= ttot
         if (it==1 || it%ndt==0) dt=dtsc*min(1.0, cfl/(epsi+maximum(D))) end
         update!(S,H,B,M,D,gradS,qHx,qHy,dHdt,dx,dy,dt,n,a,threads,blocks)
         if it%nout == 0
-            @printf("it = %d, max(dHdt) = %1.2e \n", it, maximum(dHdt[2:end-1,2:end-1]))
-            if maximum(dHdt)<ϵtol break; end
+            @printf("it = %d, t = %1.2f, max(dHdt) = %1.2e \n", it, t, maximum(dHdt[2:end-1,2:end-1]))
             p1 = heatmap(xc,yc,Array(S'), title="S, it=$(it)"; opts...)
             p2 = heatmap(xc,yc,Array(H'), title="H"; opts...)
             p3 = plot(xc, [Array(S[:,ceil(Int,ny/2)]),Array(B[:,ceil(Int,ny/2)])])
