0ba776c03fcbd7e90e69024a960cd77bf09eaa7f
yiluchen1066 <15835221066@163.com>
2023-04-14 15:42:30 +0200

    makie plot

--- makie_vis.jl
+++ makie_vis.jl
@@ -2,6 +2,7 @@ using GLMakie
 using DelimitedFiles
 using LinearAlgebra
 
+#synthetic glacier 
 nx = 128 
 ny = 128 
 
@@ -9,12 +10,13 @@ lx = 250000
 ly = 250000
 lz = 1e3
 
-dx = xc[2] - xc[1]
-dy = yc[2] - yc[1]
+
+dx = lx/nx
+dy = ly/ny
+
 
 xc = LinRange(-lx/2+dx/2, lx/2-dx/2, nx)
 yc = LinRange(-ly/2+dy/2, ly/2-dy/2, ny)
-
 
 w1 = 1e10
 w2 = 1e9 
@@ -25,11 +27,20 @@ B = zeros(Float64, nx, ny)
 
 B = @. B0*(exp(-xc^2/w1 - yc'^2/w2) + exp(-xc^2/w2-(yc'-ly/ω)^2/w1))
 
-fig = Figure(resolution=(2500,1200), fontsize=42)
-ax = Axis3(fig[1,1][1,1]; aspect_ratio=:data, title="Synthetic glacier")
+fig = Figure(resolution=(2500,25000), fontsize=42)
+ax = Axis3(fig[1,1][1,1]; aspect=:data, title="Synthetic glacier")
 
-surface!(ax, xc, yc, B; colormap=:lightterrain)
-savefig("synthetic_glacier.png", fig)
+GLMakie.surface!(ax, xc, yc, B; colormap=:lightterrain)
+#GLMakie.xlabel!(ax,"X in m")
+#GLMakie.ylabel!(ax,"Y in m")
+#GLMakie.zlabel!(ax,"Height in m")
+save("synthetic_glacier.png", fig)
 
+# syntheic 
 fig
+
+
+
+
+
 
