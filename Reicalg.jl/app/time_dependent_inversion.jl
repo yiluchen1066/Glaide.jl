618bbd306ee1b063429a7e8798a774d8b7e0541c
Ludovic Raess <ludovic.rass@gmail.com>
2024-06-26 09:55:24 +0200

    Up

--- Reicalg.jl/app/time_dependent_inversion.jl
+++ Reicalg.jl/app/time_dependent_inversion.jl
@@ -3,19 +3,6 @@ using CUDA
 using CairoMakie
 using Printf
 
-function load_from_file(path)
-    scalars, numerics = load(path, "scalars", "numerics")
-    fields = SIA_fields(numerics.nx, numerics.ny)
-
-    host_fields = load(path, "fields")
-
-    for name in intersect(keys(fields), keys(host_fields))
-        copy!(fields[name], host_fields[name])
-    end
-
-    return fields, scalars, numerics
-end
-
 function time_dependent_inversion()
     fields, scalars, numerics = load_from_file(joinpath(@__DIR__, "../datasets/synthetic/synthetic_setup.jld2"))
 
