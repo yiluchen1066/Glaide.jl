34a4ab1ee52c26ae37a6da89013d290a552f1e9d
Ivan Utkin <iutkin@ethz.ch>
2024-07-19 18:51:33 +0200

    Use new optimiser API

--- Reicalg.jl/app/time_dependent_inversion.jl
+++ Reicalg.jl/app/time_dependent_inversion.jl
@@ -2,26 +2,33 @@ using Reicalg
 using CUDA
 using CairoMakie
 using Printf
+using JLD2
 
-Base.@kwdef struct TimeDependentParams{T,I}
-    As_init::T  = 1e-22
-    E::T        = 1.0
-    β_reg::T    = 1.0e-2
-    γ::T        = 1e3
-    niter::I    = 2000
-    momentum::T = 0.5
-    ωᵥ::T       = 1.0
-    ωₕ::T       = 1.0
+Base.@kwdef struct TimeDependentInversionParams{T,I,LS}
+    # initial sliding parameter value
+    As_init::T = 1e-22
+    # ice flow enhancement factor
+    E::T = 1.0
+    # regularisation parameter
+    β_reg::T = 1.0e-3
+    # weight for velocity matching
+    ωᵥ::T = 1.0
+    # weight for ice thickness matching
+    ωₕ::T = 1.0
+    # maximum number of iterations in the optimisation algorithm
+    maxiter::I = 1000
+    # line search algorithm
+    line_search::LS
 end
 
-function time_dependent_inversion(filepath, params::TimeDependentParams)
+function time_dependent_inversion(filepath, params::TimeDependentInversionParams)
+    # unpack params
+    (; As_init, E, β_reg, ωᵥ, ωₕ, maxiter, line_search) = params
+
     model = TimeDependentSIA(filepath)
 
-    (; dx, dy, xc, yc) = model.numerics
-    (; As, V, H)       = model.fields
-
-    # unpack params
-    (; As_init, E, β_reg, γ, niter, momentum, ωᵥ, ωₕ) = params
+    (; xc, yc) = model.numerics
+    (; As, V, H) = model.fields
 
     As0 = As
 
@@ -38,8 +45,7 @@ function time_dependent_inversion(filepath, params::TimeDependentParams)
     fill!(As0, As_init)
     solve!(model)
 
-    objective      = TimeDependentObjective(ωᵥ, ωₕ, V_obs, H_obs)
-    regularisation = Regularisation(β_reg, dx, dy, As)
+    objective = TimeDependentObjective(ωᵥ, ωₕ, V_obs, H_obs, β_reg)
 
     xc_km = xc / 1e3
     yc_km = yc / 1e3
@@ -48,7 +54,7 @@ function time_dependent_inversion(filepath, params::TimeDependentParams)
 
     #! format:off
     ax = (Axis(fig[1, 1][1, 1]; aspect=DataAspect(), title="log10(As)"),
-          Axis(fig[1, 2][1, 1]; aspect=DataAspect(), title="Ās"),
+          Axis(fig[1, 2][1, 1]; aspect=DataAspect(), title="dJ/d(log10(As))"),
           Axis(fig[2, 1][1, 1]; aspect=DataAspect(), title="V_obs"),
           Axis(fig[2, 2][1, 1]; aspect=DataAspect(), title="V"),
           Axis(fig[3, 1][1, 1]; aspect=DataAspect(), title="H_obs"),
@@ -69,12 +75,11 @@ function time_dependent_inversion(filepath, params::TimeDependentParams)
           Colorbar(fig[3, 2][1, 2], hm[6]))
     #! format:on
 
-    function callback(iter, γ, J1, As, Ās)
+    function callback(iter, α, J1, logAs, logĀs)
         if iter % 10 == 0
-            @printf("  iter = %d, J = %1.3e\n", iter, J1)
-
-            hm[1][3] = Array(log10.(As))
-            hm[2][3] = Array(Ās)
+            @printf("  iter = %-4d, J = %1.3e, α = %1.3e\n", iter, J1, α)
+            hm[1][3] = Array(logAs .* log10(ℯ))
+            hm[2][3] = Array(logĀs ./ log10(ℯ))
             hm[4][3] = Array(V)
             hm[6][3] = Array(H)
             autolimits!(ax[1])
@@ -83,11 +88,20 @@ function time_dependent_inversion(filepath, params::TimeDependentParams)
         end
     end
 
-    gradient_descent(model, objective, As0, γ, niter; regularisation, callback, momentum, report=false)
+    options = OptimisationOptions(line_search, callback, maxiter)
+
+    optimise(model, objective, log.(As0), options)
 
     return
 end
 
-time_dependent_inversion("datasets/synthetic/synthetic_setup.jld2", TimeDependentParams(; As_init=1e-22, β_reg=1e-2, ωᵥ=1.0, γ=1e3, momentum=0.5))
+synthetic_params = TimeDependentInversionParams(;
+                                                line_search=BacktrackingLineSearch(; α_min=1e2, α_max=5e4))
 
-time_dependent_inversion("datasets/aletsch/aletsch_setup.jld2", TimeDependentParams(; As_init=1e-22, β_reg=1e-2, E=0.01, γ=2e2, momentum=0.2, ωₕ=1.0, ωᵥ=1.0))
+time_dependent_inversion("datasets/synthetic_setup.jld2", synthetic_params)
+
+aletsch_params = TimeDependentInversionParams(;
+                                              E           = 2.5e-1,
+                                              line_search = BacktrackingLineSearch(; α_min=1e2, α_max=1.5e4))
+
+time_dependent_inversion("datasets/aletsch_setup.jld2", aletsch_params)
