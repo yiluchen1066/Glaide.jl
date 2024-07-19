4649ca25968dd63d6353edfcd1468accb0886bf1
Ivan Utkin <iutkin@ethz.ch>
2024-07-19 18:52:14 +0200

    Add new optimisation API to Reicalg

--- Reicalg.jl/src/Reicalg.jl
+++ Reicalg.jl/src/Reicalg.jl
@@ -8,24 +8,32 @@ export TimeDependentScalars, TimeDependentNumerics, TimeDependentAdjointNumerics
 
 export Regularisation
 
-export solve!, gradient_descent
+export BacktrackingLineSearch, OptimisationOptions
+export solve!, optimise
 
+export SECONDS_IN_YEAR
+
+using LinearAlgebra
 using Printf
 using CairoMakie
 using CUDA
 using Enzyme
 using JLD2
 
+# define consntants
+const SECONDS_IN_YEAR = 3600 * 24 * 365
+
 # surface mass balance model
 ela_mass_balance(z, β, ela, b_max) = min(β * (z - ela), b_max)
 
-# CUDA launch configuration heuristics
+# CUDA launch configuration heuristics in 1D
 function launch_config(sz::Integer)
     nthreads = 256
     nblocks  = cld(sz, nthreads)
     return nthreads, nblocks
 end
 
+# CUDA launch configuration heuristics in 2D
 function launch_config(sz::NTuple{2,Integer})
     nthreads = 32, 8
     nblocks  = cld.(sz, nthreads)
@@ -39,14 +47,15 @@ const DupNN = DuplicatedNoNeed
 # helper macros for calculating finite differences
 include("macros.jl")
 
+# homogeneous and non-homogeneous Neumann boundary conditions
 include("boundary_conditions.jl")
 
+# SIA model and adjoint calls
 include("sia.jl")
 include("sia_adjoint.jl")
 
-include("regularisation.jl")
-
-include("gradient_descent.jl")
+# gradient-based optimisation
+include("optimise.jl")
 
 # snapshot inverison
 include("snapshot/parameters.jl")
