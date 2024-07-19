ff1418e0d4659de7af79ea7c6d31973648f09240
Ivan Utkin <iutkin@ethz.ch>
2024-07-19 18:54:48 +0200

    Clarify comments

--- Reicalg.jl/src/macros.jl
+++ Reicalg.jl/src/macros.jl
@@ -13,8 +13,9 @@ macro get_index_1d() esc(:( i = (blockIdx().x-1) * blockDim().x + threadIdx().x;
 macro av_xy(A) esc(:( 0.25*($A[ix, iy    ] + $A[ix + 1, iy    ] +
                             $A[ix, iy + 1] + $A[ix + 1, iy + 1]) )) end
 macro av_xa(A) esc(:( 0.5*($A[ix, iy] + $A[ix + 1, iy    ]) )) end
-macro av_ya(A) esc(:( 0.5*($A[ix, iy] + $A[ix    , iy + 1]) )) end 
-# derivatives
+macro av_ya(A) esc(:( 0.5*($A[ix, iy] + $A[ix    , iy + 1]) )) end
+
+# finite differences
 macro d_xa(A)  esc(:( $A[ix + 1, iy    ] - $A[ix    , iy    ] )) end 
 macro d_ya(A)  esc(:( $A[ix    , iy + 1] - $A[ix    , iy    ] )) end
 macro d_xi(A)  esc(:( $A[ix + 1, iy + 1] - $A[ix    , iy + 1] )) end 
