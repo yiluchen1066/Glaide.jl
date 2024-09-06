4643eac9d97a7a450550740fbfc136df5cf2506f
Ludovic Raess <ludovic.rass@gmail.com>
2024-09-06 10:21:03 +0200

    Cleanup

--- app/forward_aletsch.jl
+++ app/forward_aletsch.jl
@@ -45,40 +45,40 @@ nts = [1,2,11];
 
 # ╔═╡ 9ebf02c2-7234-4f09-8108-b671c99263cc
 models = let
-	mkpath("../output/forward_aletsch/")
-	models = Matrix{Any}(undef,2,3)
-	for (flag, inversion_file) in enumerate(inversion_files)
-		for (i, nt) in enumerate(nts)
-    		model = TimeDependentSIA(input_file; report=true)
-    		model.scalars.A *= E
+    mkpath("../output/forward_aletsch/")
+    models = Matrix{Any}(undef,2,3)
+    for (flag, inversion_file) in enumerate(inversion_files)
+        for (i, nt) in enumerate(nts)
+            model = TimeDependentSIA(input_file; report=true)
+            model.scalars.A *= E
 
-   			X = load(inversion_file, "X")
-    		copy!(model.fields.As, exp.(X))
+            X = load(inversion_file, "X")
+            copy!(model.fields.As, exp.(X))
 
-    		for it in 1:nt
-        		solve!(model)
-        		model.fields.H_old .= model.fields.H
-    		end
+            for it in 1:nt
+                solve!(model)
+                model.fields.H_old .= model.fields.H
+            end
 
-    		# save the results
-    		V = Array(model.fields.V)
-    		H = Array(model.fields.H)
+            # save the results
+            V = Array(model.fields.V)
+            H = Array(model.fields.H)
 
-			if flag==1
-				jldsave("../output/forward_aletsch/forward_aletsch_25m_snap_$(nt)yrs.jld2"; V, H)
-			else
-    			jldsave("../output/forward_aletsch/forward_aletsch_25m_time_dep_$(nt)yrs.jld2"; V, H)
-			end
+            if flag==1
+                jldsave("../output/forward_aletsch/forward_aletsch_25m_snap_$(nt)yrs.jld2"; V, H)
+            else
+                jldsave("../output/forward_aletsch/forward_aletsch_25m_time_dep_$(nt)yrs.jld2"; V, H)
+            end
             models[flag,i] = model
-		end
-	end
-	models
+        end
+    end
+    models
 end;
 
 # ╔═╡ a2a8b755-7466-4fe5-aa7a-f2043d96d04b
 with_theme(theme_latexfonts()) do
     model = models[1,1]
-	
+
     fig = Figure(size=(700, 280))
 
     axs = (Axis(fig[1,1][1,1]; aspect=DataAspect()),
@@ -105,7 +105,7 @@ with_theme(theme_latexfonts()) do
     x, y = model.numerics.xc ./ 1e3, model.numerics.yc ./ 1e3
 
     H  = Array(model.fields.H)
-	HH = copy(H)
+    HH = copy(H)
     V  = Array(model.fields.V)
     As = Array(model.fields.As)
 
@@ -119,7 +119,8 @@ with_theme(theme_latexfonts()) do
     hms = (heatmap!(axs[1], x, y, As; colorscale=log10),
            heatmap!(axs[2], x, y, V .* SECONDS_IN_YEAR),
            heatmap!(axs[3], x, y, H))
-	[contour!(axs[i], x, y, HH; levels=1:1, linewidth=0.5, color=:black) for i=1:3]
+
+    [contour!(axs[i], x, y, HH; levels=1:1, linewidth=0.5, color=:black) for i=1:3]
 
     hms[1].colormap = Reverse(:roma)
     hms[2].colormap = :matter
